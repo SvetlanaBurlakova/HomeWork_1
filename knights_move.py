797b389aa30e3a403f3062bd469011f40adb36e5
Svetlana Burlakova <svetlya4ok83@gmail.com>
2023-07-21 18:45:59 +0400

    Create README.md

---
+++ knights_move.py
@@ -0,0 +1,23 @@
+str = input()
+s=str[0]
+x = 8-int(str[1])
+symb = 'abcdefgh'
+y = symb.find(s)
+matrix = [['.']*8 for _ in range(8)]
+matrix[x][y] = 'Q'
+for i in range(8):
+    if matrix[i][y]!='Q':
+        matrix[i][y] ='*'
+    if matrix[x][i]!='Q':
+        matrix[x][i] = '*'
+
+for i in range(8):
+    for j in range(8):
+        if abs(x - i) == abs(y - j) and matrix[i][j]!='Q':
+            matrix[i][j] = '*'
+for row in matrix:
+    print(*row)
+
+
+
+
